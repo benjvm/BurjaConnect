ac9d57d4233873e3563e52a3fa01d2813cea6e13
benjamín Sánchez <benjasanchez730@gmail.com>
2026-01-30 13:29:09 +0100

    cambios en funcion calendario

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,9 +2,9 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import "/src/assets/calendar.css";
 
-export default function CalendarView() {
+export default function CalendarView({ variant = "default" }) {
   return (
-    <div className="calendar">
+    <div className={`calendar ${variant}`}>
         <FullCalendar
             plugins={[dayGridPlugin]}
             initialView="dayGridMonth"
