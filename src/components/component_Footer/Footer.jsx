74271fa7fd8a14353a4befd7a714f016123bdf55
César <cmarcillovargas@gmail.com>
2026-02-02 08:12:37 +0100

    Cambio a id en footer

--- src/components/component_Footer/Footer.jsx
+++ src/components/component_Footer/Footer.jsx
@@ -3,7 +3,7 @@ import '../../assets/styles/style_Footer/Footer.css';
 export function Footer()
 {
     return(
-        <div className="Footer_">
+        <div id ="Footer_">
             <p>&copy; 2026 - Creado y Diseñado por Benjamín, César y Paula</p>
         </div>
     );
