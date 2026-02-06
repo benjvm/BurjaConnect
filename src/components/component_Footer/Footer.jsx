4ad91e685604856fa84013bfbfa61581a72c7759
benjamín Sánchez <benjasanchez730@gmail.com>
2026-02-06 09:30:27 +0100

    Footer corregido

--- src/components/component_Footer/Footer.jsx
+++ src/components/component_Footer/Footer.jsx
@@ -1,7 +1,15 @@
 import "../../assets/styles/style_Footer/Footer.css";
 import "../../assets/styles/style_General/General.css";
 
-export default function Footer(){
+export default function App(){
+    return(
+        <div>
+            <h2>Hola mundo</h2>
+        </div>
+    )
+}
+
+export function Footer(){
     return(
         <div id="footer">
             <p>&copy; 2026 - Creado y Diseñado por Benjamín, César y Paula</p>
