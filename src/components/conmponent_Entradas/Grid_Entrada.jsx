d9f1918c7bced25e63ca5e01f45b5804ab257b43
César <cmarcillovargas@gmail.com>
2026-02-13 10:38:13 +0100

    Cambio de imagenes de las entradas

--- src/components/conmponent_Entradas/Grid_Entrada.jsx
+++ src/components/conmponent_Entradas/Grid_Entrada.jsx
@@ -7,12 +7,12 @@ export default function Grid_Entrada() {
     var [busqueda, setBusqueda] = useState("");
 
     var elementos = [
-        { imagen: "/img/bjj.jpg", titulo: "Museu de la Universitat de València d'Història Natural" , contexto: "Conoce más sobre historia en burjassot en el museo de...", bt: "Ver más"},
-        { imagen: "/img/bjj.jpg", titulo: "Iglesia Arciprestal San Miguel Arcángel", contexto: "Conoce más sobre historia en burjassot en el museo de...", bt: "Ver más" },
-        { imagen: "/img/bjj.jpg", titulo: "Museu de la Universitat de València d'Història Natural",contexto: "Conoce más sobre historia en burjassot en el museo de...", bt: "Ver más"},
-        { imagen: "/img/bjj.jpg", titulo: "The temple bar Burjassot", contexto: "Los mejores findes con los mejores amigos, conoce los mejores bares de...", bt: "Ver más"},
+        { imagen: "/img/museo_natural.jpg", titulo: "Museu de la Universitat de València d'Història Natural" , contexto: "Conoce más sobre historia en burjassot en el museo de...", bt: "Ver más"},
+        { imagen: "/img/Iglesia-Burja.jpg", titulo: "Iglesia Arciprestal San Miguel Arcángel", contexto: "Conoce más sobre historia en burjassot en el museo de...", bt: "Ver más" },
+        { imagen: "/img/Museo_Burja.jpg", titulo: "Museu de la Universitat de València d'Història Natural",contexto: "Conoce más sobre historia en burjassot en el museo de...", bt: "Ver más"},
+        { imagen: "/img/temple_bar.jpg", titulo: "The temple bar Burjassot", contexto: "Los mejores findes con los mejores amigos, conoce los mejores bares de...", bt: "Ver más"},
         { imagen: "/img/bjj.jpg", titulo: "La ruta", contexto: " LA RUTA: es un viaje sensorial a la València de los años 80 y 90. En un...", bt: "Ver más"},
-        { imagen: "/img/bjj.jpg", titulo: "The temple bar Burjassot", contexto: "Los mejores findes con los mejores amigos, conoce los mejores bares de...", bt: "Ver más"}
+        { imagen: "/img/bar.jpg", titulo: "The temple bar Burjassot", contexto: "Los mejores findes con los mejores amigos, conoce los mejores bares de...", bt: "Ver más"}
     ];
 
     var elementosFiltrados = elementos.filter((item) =>
